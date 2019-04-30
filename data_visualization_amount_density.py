e2e1869f2dfaa0289aa3f4bf5fedd2c394954ef7
bianca26 <biby_iancu26@yahoo.com>
2019-04-30 10:55:44 +0200

    visualization for density estimation amount

---
+++ data_visualization_amount_density.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 26 09:51:53 2019
+
+@author: Bianca Iancu
+"""
+
+
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("C:\\Users\\biby_\\Desktop\\TU Delft\\First year\\Period 4\\Cyber Data Analytics\\Assignment 1\\data_for_student_case.csv")
+
+data_fraud = df.loc[df['simple_journal'] == 'Chargeback']
+data_no_fraud = df.loc[df['simple_journal'] == 'Settled']
+data_refused = df.loc[df['simple_journal'] == 'Refused']
+
+sns.set_style('whitegrid')
+ax = sns.distplot(np.array(data_fraud['amount']), hist = False, label = 'Chargeback', color = 'red')
+
+sns.set_style('whitegrid')
+ax = sns.distplot(np.array(data_no_fraud['amount']), hist = False, label = 'Settled', color = 'lightgreen')
+
+sns.set_style('whitegrid')
+ax = sns.distplot(np.array(data_refused['amount']), hist = False, label = 'Refused', color = 'lightgrey')
+
+ax.set(xlabel='Transaction amount')
+
+plt.savefig("density_estimation_amount.pdf", format = 'pdf')
