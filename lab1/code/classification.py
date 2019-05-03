154400899b88d55595853513dfc926ca65c44d72
Gabriele Mazzola <gabriele.mazzola@techedgegroup.com>
2019-05-03 13:23:03 +0200

    task 2 lab 1

--- lab1/code/classification.py
+++ lab1/code/classification.py
@@ -1,15 +1,14 @@
-from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import SMOTE, BorderlineSMOTE
 import pandas as pd
-import seaborn as sns
-import numpy as np
 import matplotlib.pyplot as plt
-import os
-import zipfile
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+
 from sklearn import metrics
+from sklearn.metrics import roc_curve, roc_auc_score, precision_recall_curve
 
 DATA_PATH = r"../data/"
 DATA_FILE = "data_processed.csv"
@@ -21,82 +20,75 @@ y = df['label']
 X = df.drop(columns = ['label'])
 
 #We first split in train and test set -> we only have to apply SMOTE on train set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 
 #Apply SMOTE
-print(f"Fraud cases: {len(y_train[y_train==1])} and non-fraud cases: {len(y_train[y_train==0])}")
+print(f"Before SMOTE: Fraud cases train: {len(y_train[y_train==1])} and non-fraud cases train: {len(y_train[y_train==0])}")
+print(f"Before SMOTE: Fraud cases test: {len(y_test[y_test==1])} and non-fraud cases train: {len(y_test[y_test==0])}")
 
+brd_smote = BorderlineSMOTE()
 smt = SMOTE()
 X_smote, y_smote = smt.fit_sample(X_train, y_train)
 
-print(f"Fraud cases: {len(y_smote[y_smote == 1])} and non-frau cases: {len(y_smote[y_smote ==0 ])}")
+print(f"After SMOTE: Fraud cases train: {len(y_smote[y_smote == 1])} and non-fraud cases: {len(y_smote[y_smote ==0 ])}")
+print(f"After SMOTE: Fraud cases test: {len(y_test[y_test==1])} and non-fraud cases train: {len(y_test[y_test==0])}")
 
 
-#Try some classification
-print('---------- CLASSIFICATION BEFORE SMOTE----------')
+classifiers = [
+    ("Knn", KNeighborsClassifier(n_neighbors=7)),
+    ("Random Forest", RandomForestClassifier(n_estimators=150)),
+    ("Logistic Regression", LogisticRegression()),
+    #("SVM", svm.SVC())
+]
 
-print('\n Random Forest')
-rf=RandomForestClassifier(n_estimators=100)
-rf.fit(X_train,y_train)
-y_pred_rf=rf.predict(X_test)
+for model_name, model in classifiers:
+    print(f'---------- {model_name} BEFORE SMOTE----------')
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
 
-print("Accuracy RF:", metrics.accuracy_score(y_test, y_pred_rf))
-print("Recall RF:", metrics.recall_score(y_test, y_pred_rf))
-print("ROC AUC Score RF:",metrics.roc_auc_score(y_test, y_pred_rf))
-print(metrics.confusion_matrix(y_test, y_pred_rf))
+    probs = model.predict_proba(X_test)
+    probs = probs[:, 1]  # only positive class
+    fpr_unsmoted, tpr_unsmoted, _ = roc_curve(y_test, probs)
+    precision_unsmoted, recall_unsmoted, _ = precision_recall_curve(y_test, probs)
 
+    print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+    print("Recall:", metrics.recall_score(y_test, y_pred))
+    print("Precision:", metrics.precision_score(y_test, y_pred))
+    print("F1: ", metrics.f1_score(y_test, y_pred))
+    print("ROC AUC Score:", metrics.roc_auc_score(y_test, y_pred))
+    print(metrics.confusion_matrix(y_test, y_pred))
 
-print('\n Logistic Regression')
-lr = LogisticRegression()
-lr.fit(X_train, y_train)
-y_pred_lr = lr.predict(X_test)
+    print(f'---------- {model_name} AFTER SMOTE----------')
+    model.fit(X_smote, y_smote)
+    y_pred = model.predict(X_test)
 
-print("Accuracy log reg:", metrics.accuracy_score(y_test, y_pred_lr))
-print("Recall log reg:", metrics.recall_score(y_test, y_pred_lr))
-print("ROC AUC Score log reg:",metrics.roc_auc_score(y_test, y_pred_lr))
-print(metrics.confusion_matrix(y_test, y_pred_lr))
+    probs_smoted = model.predict_proba(X_test)
+    probs_smoted = probs_smoted[:, 1]
+    fpr_smoted, tpr_smoted, _ = roc_curve(y_test, probs_smoted)
+    precision_smoted, recall_smoted, _ = precision_recall_curve(y_test, probs_smoted)
 
+    # Model Accuracy, how often is the classifier correct?
+    print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+    print("Recall:", metrics.recall_score(y_test, y_pred))
+    print("Precision:", metrics.precision_score(y_test, y_pred))
+    print("F1: ", metrics.f1_score(y_test, y_pred))
+    print("ROC AUC Score:", metrics.roc_auc_score(y_test, y_pred))
+    print(metrics.confusion_matrix(y_test, y_pred))
 
-print('\n Support Vector Machine')
-svm = svm.SVC()
-svm.fit(X_train, y_train)
-y_pred_svm = svm.predict(X_test)
-print("Accuracy SVM:", metrics.accuracy_score(y_test, y_pred_svm))
-print("Recall SVM:", metrics.recall_score(y_test, y_pred_svm))
-print("ROC AUC Score SVM:",metrics.roc_auc_score(y_test, y_pred_svm))
-print(metrics.confusion_matrix(y_test, y_pred_svm))
+    plt.plot(fpr_unsmoted, tpr_unsmoted, color='blue', label='UNSMOTEd')
+    plt.plot(fpr_smoted, tpr_smoted, color='red', label='SMOTEd')
+    plt.plot([0, 1], [0, 1], color='darkblue', linestyle='--')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(f'ROC Curves - {model_name}')
+    plt.legend()
+    plt.show()
 
-
-print('---------- CLASSIFICATION AFTER SMOTE----------')
-
-print('\n Random Forest')
-clf=RandomForestClassifier(n_estimators=100)
-clf.fit(X_smote,y_smote)
-y_pred=clf.predict(X_test)
-
-# Model Accuracy, how often is the classifier correct?
-print("Accuracy RF:",metrics.accuracy_score(y_test, y_pred))
-print("Recall RF:", metrics.recall_score(y_test, y_pred))
-print("ROC AUC Score RF:",metrics.roc_auc_score(y_test, y_pred))
-print(metrics.confusion_matrix(y_test, y_pred))
-
-print('\n Logistic Regression')
-lr = LogisticRegression()
-lr.fit(X_smote, y_smote)
-y_pred_lr = lr.predict(X_test)
-
-print("Accuracy log reg:", metrics.accuracy_score(y_test, y_pred_lr))
-print("Recall log reg:", metrics.recall_score(y_test, y_pred_lr))
-print("ROC AUC Score log reg:",metrics.roc_auc_score(y_test, y_pred_lr))
-print(metrics.confusion_matrix(y_test, y_pred_lr))
-
-
-print('\n Support Vector Machine')
-svm = svm.SVC()
-svm.fit(X_smote, y_smote)
-y_pred_svm = svm.predict(X_test)
-print("Accuracy SVM:", metrics.accuracy_score(y_test, y_pred_svm))
-print("Recall SVM:", metrics.recall_score(y_test, y_pred_svm))
-print("ROC AUC Score SVM:",metrics.roc_auc_score(y_test, y_pred_svm))
-print(metrics.confusion_matrix(y_test, y_pred_svm))
+    plt.plot(recall_unsmoted, precision_unsmoted, color='blue', label='UNSMOTEd')
+    plt.plot(recall_smoted, precision_smoted, color='red', label='SMOTEd')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title(f'PR Curves - {model_name}')
+    plt.legend()
+    plt.show()
